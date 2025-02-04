b53fae45df5133cff873779355c12dd720b8a966
josiasgdev <josiasdev97@gmail.com>
2025-02-04 03:48:17 -0300

    A biblioteca PHPMailer foi adicionada ao projeto e foi adicionado um bloco que instancia um objeto da classe PHPMailer.

--- processa_envio.php
+++ processa_envio.php
@@ -1,39 +1,92 @@
-<?php 
+<?php
+//Import dos arquivos da biblioteca PHPMailer
+require "./bibliotecas/PHPMailer/DSNConfigurator.php";
+require "./bibliotecas/PHPMailer/Exception.php";
+require "./bibliotecas/PHPMailer/OAuthTokenProvider.php";
+require "./bibliotecas/PHPMailer/OAuth.php";
+require "./bibliotecas/PHPMailer/PHPMailer.php";
+require "./bibliotecas/PHPMailer/POP3.php";
+require "./bibliotecas/PHPMailer/SMTP.php";
 
-    class Mensagem {
-        private $para = null;
-        private $assunto = null;
-        private $mensagem = null;
+//Import dos namespaces
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Excpetion;
 
 
-        //Métodos mágicos
-        //Get
-        public function __get($atributo){
-            return $this->$atributo;
-        }
-        //Set
-        public function __set($atributo, $valor){
-            $this->$atributo = $valor;
-        }
-        //Método verifica se algum dos campos não foi preenchido
-        public function MensagemValida(){
-           if(empty($this->para)||empty($this->assunto)||empty($this->mensagem)){
-                return false;
-           }
-           return true;
-        }
+
+class Mensagem
+{
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+
+    //Métodos mágicos
+    //Get
+    public function __get($atributo)
+    {
+        return $this->$atributo;
     }
-
-    $mensagem = new mensagem();
-
-    $mensagem->__set('para', $_POST['para']);
-    $mensagem->__set('assunto', $_POST['assunto']);
-    $mensagem->__set('mensagem', $_POST['mensagem']);
-
-    if($mensagem->MensagemValida()){
-        echo 'Mensagem é válida';
-    } else {
-        echo 'Mensagem não é válida';
+    //Set
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
     }
+    //Método verifica se algum dos campos não foi preenchido
+    public function MensagemValida()
+    {
+        if (empty($this->para) || empty($this->assunto) || empty($this->mensagem)) {
+            return false;
+        }
+        return true;
+    }
+}
 
-?>
+$mensagem = new mensagem();
+
+$mensagem->__set('para', $_POST['para']);
+$mensagem->__set('assunto', $_POST['assunto']);
+$mensagem->__set('mensagem', $_POST['mensagem']);
+
+if (!$mensagem->MensagemValida()) {
+    echo 'Mensagem não é válida';
+    die();
+}
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'user@example.com';                     //SMTP username
+    $mail->Password   = 'secret';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+    $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Não foi possível enviar este e-mail. Por favor, tente novamente mais tarde!';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
